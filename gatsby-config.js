a887d2eb0eedad1cccc6bb99d28339d6ac58622a
weezykon <weezykon@gmail.com>
2018-04-28 01:35:50 +0100

    add images plugin, transformer sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Welcome To Weezykon',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ],
 }
